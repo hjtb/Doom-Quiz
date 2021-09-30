41c168f7e2790149ad135b5c3885e3167cc637c9
hjtb <willholtonjtb@gmail.com>
2021-09-30 07:43:55 +0000

    Added event listeners to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function() {
-
+            button.addEventListener("click", function() {
+                if (this.getAttribute("data-type") === "answer") {
+                    let answer = this.getAttribute("id");
+                    alert(`You selected ${answer}!`);
+                }else {
+                    let difficultyLevel = this.getAttribute("data-type");
+                    alert(`You clicked ${difficultyLevel}`);
+                }
+            })
         })
     }
 })
