298b33a94980c6441d63d6f2668ae67d2bc22ca3
hjtb <willholtonjtb@gmail.com>
2021-10-02 11:54:34 +0000

    querying the api for the first time

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+let questionFile;
 let gameArea = document.getElementById("game-area");
 let openMenu = document.getElementById("open-menu"); 
 let startButtonHard = document.getElementById("hurt-me");
@@ -24,8 +25,8 @@ startButtonEasy.addEventListener('click', start);
 // add a transition where the title moves into the game area
 
 function start(difficultyLevel) {
-    $(gameArea).slideDown('slow');
-    $(".title").css({'width':'40vw', 'max-width':'20rem', 'margin':'30px auto'});
+    $(gameArea).slideDown('slow').prepend($('.title'));
+    $(".title").css({'width':'40vw', 'max-width':'20rem', 'margin':'10px auto'});
     $(openMenu).slideUp('fast');
     if (difficultyLevel === "easy") {
         console.log(difficultyLevel);
@@ -35,10 +36,17 @@ function start(difficultyLevel) {
         console.log(difficultyLevel);
         throw `Unknown difficulty: ${difficultyLevel}. Aborting!`;
     }
+    getQuestion();
 }
 
 function getQuestion() {
-
+    fetch("./questions.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
+    let displayQuestion = document.getElementById("question").innerHTML;
+    displayQuestion = data["questions"][0]["question"];
 }
 
 function selectAnswer() {
