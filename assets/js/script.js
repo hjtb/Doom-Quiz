b671095fa24d777f054af9ce38a48d6771974d12
hjtb <willholtonjtb@gmail.com>
2021-10-08 11:31:53 +0000

    added more questions and added lives feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 //when the dom has loaded add event listeners to the buttons
-var globalQuestions = 1;
-var difficultyLevel;
-var questionIndex = 0;
+let globalQuestions = 1;
+let difficultyLevel;
+let questionIndex = 0;
+let lives = 3;
+let streak = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -20,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let gameArea = document.getElementById("game-area");
 let openMenu = document.getElementById("open-menu"); 
+let livesLeft = document.getElementById("lives");
 
 // set the difficulty and bring our game area in to replace the initial menu after difficulty has been selected
 function start(difficultyLevel) {
@@ -27,9 +30,9 @@ function start(difficultyLevel) {
     $(".title").css({'width':'40vw', 'max-width':'20rem', 'margin':'10px auto'});
     $(openMenu).slideUp('fast');
     if (difficultyLevel === "easy") {
-        console.log(difficultyLevel);
+        //console.log(difficultyLevel);
     }else if (difficultyLevel === "hard") {
-        console.log(`should be hard - ${difficultyLevel}`);
+        //console.log(`should be hard - ${difficultyLevel}`);
     }else {
         throw `Unknown difficulty: ${difficultyLevel}. Aborting!`;
     }
@@ -38,15 +41,18 @@ function start(difficultyLevel) {
 }
 
 //use our difficulty level to retrieve correct questions
-//comment this function so you fully understand it
 function getQuestions(difficultyLevel) {
+    // fetch will get the data with a promise 
     fetch("./questions.json")
+    // then we pass the response from the fetch into an anonymous function that parses the data
     .then(response => response.json())
+    // we then pass the result from that function into another anonymous function
     .then(questions => {
+        // this will set our globalQuestions as the result
         globalQuestions = questions;
-        console.log(questions);
+        // we then use our difficultyLevel to get the correct question set from our globalQuestions which we named levelQuestions
         let levelQuestions = questions[`${difficultyLevel}Questions`];
-        console.log(levelQuestions);
+        //finally we pass levelQuestions into updateQuestion and then displayQuestion
         updateQuestion(levelQuestions);
         displayQuestion();
     })
@@ -59,13 +65,9 @@ function shuffle(array) {
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
-
+//i've a feeling I dont need this function now but, I'm hesitant to delete until all functionality has been written
 function updateQuestion(levelQuestions) {
-    console.log(levelQuestions);
-    //credit this
     shuffle(levelQuestions)
-    console.log(`shuffled questions`);
-    console.log(levelQuestions);
 }
 
 function displayQuestion() {
@@ -85,6 +87,7 @@ function displayQuestion() {
     answerB.innerHTML = currentQuestion.answers[1];
     answerC.innerHTML = currentQuestion.answers[2];
     answerD.innerHTML = currentQuestion.answers[3];
+    livesLeft.innerHTML = lives;
     displayQuestion.innerHTML = questionText;
 }
 
@@ -99,15 +102,18 @@ function checkAnswer(answerText) {
         alert("Please choose a valid answer!")
     }
     getNextQuestion();
-    console.log(answerText)
+    //console.log(answerText)
     console.log(globalQuestions);
 }
 
 function correctAnswer(answer) {
+    streak = streak + 1;
     alert(`${answer} is Correct!! You got it right!`);
 }
 
 function wrongAnswer(answer) {
+    lives = lives - 1;
+    streak = 0;
     alert(`${answer} is Wrong!! You die a little!`);
 }
 
@@ -119,3 +125,4 @@ function getNextQuestion() {
 // add scores/lives functionality
 // what to do at end of questions/game?
 // fix answer buttons to lose the weird outline after selection
+// create a card to hold the face similar to the image used now
